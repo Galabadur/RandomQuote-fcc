9e5f66aead893b7e1e610da84036e05d472648e5
VictorUniKilo <43604130+Wookolentze@users.noreply.github.com>
2019-10-30 01:05:20 +0800

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+
 
 class Header extends Component {
 
@@ -7,7 +7,6 @@ class Header extends Component {
     render() {
         return(
             <div className="Header">
-                <img className="logo" width="30%" src={logo} alt="logo" />
                 <h1>Hello World!!!</h1>
                 <p>THIS IS A RANDOM QUOTE</p>
             </div>
